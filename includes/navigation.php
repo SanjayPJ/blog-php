bcc2a19c2b457905fb459f56e536e91132566306
sanjaypj <sanjaypjayan2000@gmail.com>
2018-10-08 12:07:34 -0700

    search part added

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,8 +1,17 @@
 <div class="col-4">
-  <h3>BLOG</h3>
-  <ul class="list-inline">
-      <li class="list-inline-item"><a href="/">Home</a></li>
-      <?php 
+    <h3>BLOG</h3>
+    <form method="get" action="search.php">
+        <div class="input-group mb-3 mt-3">
+            <input name="s" type="text" value="<?php echo htmlspecialchars($_GET['s'])?>" class="form-control" aria-describedby="button-addon2">
+
+            <div class="input-group-append">
+                <button class="btn btn-primary" type="submit" id="button-addon2">Search</button>
+            </div>
+        </div>
+    </form>
+    <ul class="list-inline">
+        <li class="list-inline-item"><a href="/">Home</a></li>
+        <?php 
         $query = "SELECT * FROM categories";
         $select_all_categories = mysqli_query($conn, $query);
 
@@ -12,4 +21,5 @@
         }
       ?>
     </ul>
+    <?php include "includes/widget.php" ?>
 </div>
--- index.php
+++ index.php
@@ -1,15 +1,39 @@
 <!doctype html>
 <html lang="en">
-    <div class="alert alert-warning p-1">
-        <?php include "includes/db_connect.php" ?>
-    </div>
-     <?php include "includes/header.php" ?>
-  <body>
-      <div class="container mt-3">
-          <?php include "includes/navigation.php" ?>
-          <div class="col-8">
+<div class="alert alert-warning p-1">
+    <?php include "includes/db_connect.php" ?>
+</div>
+<?php include "includes/header.php" ?>
 
-          </div>
-      </div>  
-  </body>
+<body>
+    <div class="container mt-3">
+        <div class="row">
+            <?php include "includes/navigation.php" ?>
+            <div class="col-8">
+                <?php 
+                $query = "SELECT * FROM posts";
+                $select_all_post_query = mysqli_query($conn, $query);
+                while($row = mysqli_fetch_assoc($select_all_post_query)){
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_content = $row['post_content'];
+                ?>
+                <div class="card p-3 mb-2">
+                   <img class="img-responsive mb-2 rounded" src="assets/img/<?php echo $post_image ?>" alt="">
+                    <h3><a href=""><?php echo $post_title ?></a></h3>
+                    <h6 class="text-muted">By <b><?php echo $post_author ?></b> at <?php echo $post_date ?></h6>
+                    <p class="text-muted"><?php echo substr($post_content, 0, 230) ?> ... <a href="">Read More >></a></p>
+                </div>
+                <?php
+                    
+                }
+            ?>
+
+            </div>
+        </div>
+    </div>
+</body>
+
 </html>
